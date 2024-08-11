b52176fb98dd3c2f70d1cf8cb0e055a8011c9975
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-08-11 19:02:09 -0300

    feat: add ts-jest

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,8 +1,12 @@
 import sum from "../src/sum";
 
-describe("Sum opetation", () => {
-    it("Should be able to sum A  and B")
-    const result:number =  sum(8, 2);
+describe("Sum operations tests", () => {
+  it("Should be able to sum A  and B", () => {
+    expect(sum(8, 2)).toBe(10);
+  });
 
-    expect(result).toBe(10);
-})
+  it("Should be able to sum A  and B", () => {
+    expect(sum(12, 80)).toBe(92);
+  });
+
+});
